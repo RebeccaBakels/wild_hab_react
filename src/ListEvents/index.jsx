1bd40b377d70ccc51c2b40caa938386327f6716b
Rebecca Bakels <rebeccabakels@gmail.com>
2021-02-12 15:04:04 -0500

    added favorites array

--- src/ListEvents/index.jsx
+++ src/ListEvents/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Col, Row, List } from 'antd';
 
-const addToFavorites = (eventId) => {
-    console.log('button clicked', eventId)
+const addToFavorites = (eventId, setFavoriteEvents) => {
+    setFavoriteEvents(favorites => favorites.concat(eventId))
 }
+
 const ListEvents = () => {
     //store events in state
-    const [eventsList, setEventsList] = useState([]) 
+    const [eventsList, setEventsList] = useState([])
+    const [favoriteEvents, setFavoriteEvents] = useState([])
 
     useEffect(() => {
     //api call get events
@@ -16,41 +18,34 @@ const ListEvents = () => {
         .catch(error => console.log('error', error))
     }, [])
     
-
     // eventName: '',
     // sport: '',
     // eventDuration: 0
     //shape
-
     
-    //display events
-    console.log(eventsList)
     return(
-        <>
-    
-    <Row justify="space-around">
-        <Col xs={24} sm={12}>
-            <h1 style={{textAlign:"center"}}>Hello List Events</h1>
-        </Col>
+    <>  
+        <Row justify="space-around">
+            <Col xs={24} sm={12}>
+                <h1 style={{textAlign:"center"}}>Hello List Events</h1>
+            </Col>
         </Row>
         <Row justify="space-around">
-        <Col >
-        <List
-         dataSource={eventsList}
-             renderItem={event => 
-                <List.Item 
-                key={event.id}
-                actions={[<a key="add-to-favorites" onClick={addToFavorites.bind(undefined, event.id)}>add to favorites</a>]}
-                >
-                    {event.eventName || event.name}, {event.sport}, Duration: {event.eventDuration}
-                </List.Item>
-                }
-        />
-        </Col>
+            <Col >
+                <List
+                    dataSource={eventsList}
+                    renderItem={event => 
+                    <List.Item 
+                        key={event.id}
+                        actions={[<a key="add-to-favorites" onClick={() => addToFavorites(event.id, setFavoriteEvents)}>add to favorites</a>]}
+                        >
+                        {event.eventName || event.name}, {event.sport}, Duration: {event.eventDuration}
+                    </List.Item>
+                    }
+                />
+            </Col>
         </Row>
-    
-        </>
-    )
-}
+    </>
+)}
 
 export default ListEvents
